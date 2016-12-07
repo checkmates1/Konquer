fca9f5f6898399c0655db23fed837a9087d93618
Ryan Wooff <ryanwooff@gmail.com>
2016-12-06 21:52:07 -0800

    fix conflicts
    removed conflicting lines of code int the HEAD sections

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161206021535) do
-=======
 ActiveRecord::Schema.define(version: 20161205164755) do
->>>>>>> master
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "games", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
-=======
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,5 +36,4 @@ ActiveRecord::Schema.define(version: 20161205164755) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> master
 end
