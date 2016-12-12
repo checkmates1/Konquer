96feaf3b366eae5e6738eb79390c53eb34e2fbff
Ryan Wooff <ryanwooff@gmail.com>
2016-12-12 03:12:40 +0000

    created enum for status/color

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211183544) do
+ActiveRecord::Schema.define(version: 20161212031021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20161211183544) do
     t.integer  "row"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status"
+    t.integer  "color"
   end
 
   create_table "users", force: true do |t|
