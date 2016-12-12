3572d91446bc465cf7132bff44f448d2f2dc06f5
Rosie <ariel.highsmith@gmail.com>
2016-12-12 13:26:19 -0800

    Added default status of alive to pieces

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212031021) do
+ActiveRecord::Schema.define(version: 20161212210417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20161212031021) do
     t.integer  "row"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "status"
+    t.integer  "status",     default: 1
     t.integer  "color"
   end
 
