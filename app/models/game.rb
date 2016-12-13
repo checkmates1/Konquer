c856544bbf01fb1a29f112acbe85813598f79ab7
David Stonikas <dstonikas@gmail.com>
2016-12-13 18:59:07 +0000

    Added Single Table Inheritence

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,6 @@ class Game < ActiveRecord::Base
   belongs_to :black_player
   belongs_to :white_player
   belongs_to :active_player
+
+  delegate :pawns, :rooks, :knights, :bishops, :queens, :kings, to: :pieces
 end
