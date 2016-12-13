cd2a7e69aad9e9e1d788c7658519fdb3dfe8d277
David Stonikas <dstonikas@gmail.com>
2016-12-13 19:26:36 +0000

    Fixed style

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,15 +6,14 @@ class Piece < ActiveRecord::Base
 
   validates :type, presence: true
 
-  scope :pawns, -> { where(type: 'Pawn')}
-  scope :rooks, -> { where(type: 'Rook')}
-  scope :knights, -> { where(type: 'Knight')}
-  scope :bishops, -> { where(type: 'Bishop')}
-  scope :queens, -> { where(type: 'Queen')}
-  scope :kings, -> { where(type: 'King')}
+  scope :pawns, -> { where(type: 'Pawn') }
+  scope :rooks, -> { where(type: 'Rook') }
+  scope :knights, -> { where(type: 'Knight') }
+  scope :bishops, -> { where(type: 'Bishop') }
+  scope :queens, -> { where(type: 'Queen') }
+  scope :kings, -> { where(type: 'King') }
 
   def types
     %w(Pawn Rook Knight Bishop Queen King)
   end
-  
 end
