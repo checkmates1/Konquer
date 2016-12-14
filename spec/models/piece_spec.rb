2b454b1e9d3a3aca753f7dcc1b1041abe03b651d
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-14 18:51:24 -0500

    adds initial tests for obstruct method

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,8 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  describe "horizontal obstruction" do
-    it "should return false if horizontally obstructed" do
+  describe "obstructed method" do
+    it "should return nil if invalid move" do
+      piece = Piece.create(row: 5, column: 5)
+      expect(piece.obstructed?(1,3)).to eq nil
     end
+
+    it "should return true if obstructed on right" do
+      pieceA = Piece.create(row: 1, column: 1)
+      pieceB = Piece.create(row: 3, column: 1)
+      expect(pieceA.obstructed?(5,1)).to eq true
+    end
+
+    it "should return true if obstructed on left" do
+      pieceA = Piece.create(row: 5, column: 1)
+      pieceB = Piece.create(row: 3, column: 1)
+      expect(pieceA.obstructed?(1,1)).to eq true
+    end
+
+    it "should return true if obstructed on bottom" do
+      pieceA = Piece.create(row: 1, column: 1)
+      pieceB = Piece.create(row: 1, column: 3)
+      expect(pieceA.obstructed?(1,5)).to eq true
+    end
+
+    it "should return true if obstructed on top" do
+      pieceA = Piece.create(row: 1, column: 5)
+      pieceB = Piece.create(row: 1, column: 3)
+      expect(pieceA.obstructed?(1,1)).to eq true
+    end
+
+    it "should return true if obstructed diagonal down-right" do
+      pieceA = Piece.create(row: 1, column: 1)
+      pieceB = Piece.create(row: 3, column: 3)
+      expect(pieceA.obstructed?(5,5)).to eq true
+    end
+
+    it "should return true if obstructed diagonal up-left" do
+      pieceA = Piece.create(row: 5, column: 5)
+      pieceB = Piece.create(row: 3, column: 3)
+      expect(pieceA.obstructed?(1,1)).to eq true
+    end
+
   end
 end
