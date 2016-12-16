de2abca24d74868573616c153cb938f5c5b919f9
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-16 05:14:21 -0500

    rewrites horizontal tests, only creates one piece per test, expects to return position of obstruction, comments out other tests

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -7,40 +7,38 @@ RSpec.describe Piece, type: :model do
       expect(piece.obstructed?(1,3)).to eq nil
     end
 
-    it "should return true if obstructed on right" do
-      pieceA = Piece.create(x_position: 1, y_position: 1)
-      pieceB = Piece.create(x_position: 3, y_position: 1)
-      expect(pieceA.obstructed?(5,1)).to eq true
+    it "should return obstruction position if obstructed on right" do
+      piece = Piece.create(x_position: 0, y_position: 2)
+      expect(piece.obstructed?(4,2)).to eq "2, 2"
     end
 
-    it "should return true if obstructed on left" do
-      pieceA = Piece.create(x_position: 5, y_position: 1)
-      pieceB = Piece.create(x_position: 3, y_position: 1)
-      expect(pieceA.obstructed?(1,1)).to eq true
+    it "should return obstruction position if obstructed on left" do
+      piece = Piece.create(x_position: 4, y_position: 2)
+      expect(piece.obstructed?(0,2)).to eq "2, 2"
     end
 
-    it "should return true if obstructed on bottom" do
-      pieceA = Piece.create(x_position: 1, y_position: 1)
-      pieceB = Piece.create(x_position: 1, y_position: 3)
-      expect(pieceA.obstructed?(1,5)).to eq true
-    end
-
-    it "should return true if obstructed on top" do
-      pieceA = Piece.create(x_position: 1, y_position: 5)
-      pieceB = Piece.create(x_position: 1, y_position: 3)
-      expect(pieceA.obstructed?(1,1)).to eq true
-    end
-
-    it "should return true if obstructed diagonal down-right" do
-      pieceA = Piece.create(x_position: 1, y_position: 1)
-      pieceB = Piece.create(x_position: 3, y_position: 3)
-      expect(pieceA.obstructed?(5,5)).to eq true
-    end
-
-    it "should return true if obstructed diagonal up-left" do
-      pieceA = Piece.create(x_position: 5, y_position: 5)
-      pieceB = Piece.create(x_position: 3, y_position: 3)
-      expect(pieceA.obstructed?(1,1)).to eq true
-    end
+    # it "should return true if obstructed on bottom" do
+    #   pieceA = Piece.create(x_position: 1, y_position: 1)
+    #   pieceB = Piece.create(x_position: 1, y_position: 3)
+    #   expect(pieceA.obstructed?(1,5)).to eq true
+    # end
+    #
+    # it "should return true if obstructed on top" do
+    #   pieceA = Piece.create(x_position: 1, y_position: 5)
+    #   pieceB = Piece.create(x_position: 1, y_position: 3)
+    #   expect(pieceA.obstructed?(1,1)).to eq true
+    # end
+    #
+    # it "should return true if obstructed diagonal down-right" do
+    #   pieceA = Piece.create(x_position: 1, y_position: 1)
+    #   pieceB = Piece.create(x_position: 3, y_position: 3)
+    #   expect(pieceA.obstructed?(5,5)).to eq true
+    # end
+    #
+    # it "should return true if obstructed diagonal up-left" do
+    #   pieceA = Piece.create(x_position: 5, y_position: 5)
+    #   pieceB = Piece.create(x_position: 3, y_position: 3)
+    #   expect(pieceA.obstructed?(1,1)).to eq true
+    # end
   end
 end
