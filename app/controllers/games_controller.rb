81ad9d79c1c100c2fa88065c0a03d5ee673aead7
David Stonikas <dstonikas@gmail.com>
2016-12-16 22:21:57 +0000

    Added feature for users to join games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,4 +10,16 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update_attributes(game_attributes)
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def game_attributes
+    params.require(:game).permit(:white_player_id, :black_player_id)
+  end
 end
