8e7a18b66fb3c8ff8046de601dab73d3ab0936ea
Rosie <ariel.highsmith@gmail.com>
2016-12-17 13:43:36 -0800

    Expect page to have available game names

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,5 +5,6 @@ RSpec.feature 'All games are listed on the homepage' do
     FactoryGirl.create(:game, name: 'Test Game')
     visit root_path
     expect(page).to have_css('.game')
+    expect(page).to have_text('Test Game')
   end
 end
