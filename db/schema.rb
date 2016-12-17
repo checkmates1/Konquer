566763836f2538674ede490fb8807880dc4d5f0b
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-17 12:56:29 -0500

    adds game id to pieces

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216223845) do
+ActiveRecord::Schema.define(version: 20161217174253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20161216223845) do
     t.datetime "updated_at"
     t.integer  "status",     default: 1
     t.integer  "color"
+    t.integer  "game_id"
   end
 
   add_index "pieces", ["type"], name: "index_pieces_on_type", using: :btree
