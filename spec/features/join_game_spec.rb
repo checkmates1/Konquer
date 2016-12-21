0ffa7827b0ba9a7f6d6f4908d6973dd1cda1013f
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-21 10:43:45 -0500

    fixes white player id, changes expectation for link click

--- spec/features/join_game_spec.rb
+++ spec/features/join_game_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.feature 'join a game' do
   let(:user1) { FactoryGirl.create(:user) }
   let(:user2) { FactoryGirl.create(:user) }
-  let(:game) { FactoryGirl.create(:game, white_player_id: user1) }
+  let(:game) { FactoryGirl.create(:game, white_player_id: user1.id) }
 
   context 'when available' do
     it 'allows user to join' do
       login_as(user2, scope: :user)
       visit game_path(game)
-      expect(page).to have_text('Only one more player needed!')
+      expect(page).to have_text 'Only one more player needed!'
 
       find_link('Join Game', visible: :all).visible?
       click_link('Join Game')
-      expect(response).to redirect_to game_path(game)
+      expect(page).to have_text 'Game is full'
 
       game.reload
       expect(game.black_player_id).to eq user2.id
