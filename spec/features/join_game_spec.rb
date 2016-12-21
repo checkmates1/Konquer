f98ed55dfb6921419f442501e834cffffedfb52e
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-21 10:24:21 -0500

    renames feature, adds context, removes comments

--- spec/features/join_game_spec.rb
+++ spec/features/join_game_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature 'Joining a game' do
+RSpec.feature 'join a game' do
   let(:user1) {FactoryGirl.create(:user)}
   let(:user2) {FactoryGirl.create(:user)}
   let(:game) {FactoryGirl.create(:game, white_player_id: user1)}
 
-  it 'join open game' do
-    login_as(user2, :scope => :user)
+  context 'when available' do
+    it 'allows user to join' do
+      login_as(user2, :scope => :user)
+      visit game_path(game)
+      expect(page).to have_text('Only one more player needed!')
 
-    visit game_path(game)
-    #expect(page).to have_text('Only one more player needed!')
-    find_link('Join Game', :visible => :all).visible?
+      find_link('Join Game', :visible => :all).visible?
+      click_link('Join Game')
+      expect(response).to redirect_to game_path(game)
 
-    click_link('Join Game')
-    #expect(response).to redirect_to game_path(game)
-
-    game.reload
-    expect(game.black_player_id).to eq user2.id
-
+      game.reload
+      expect(game.black_player_id).to eq user2.id
+    end
   end
-
-
 end
