9fddf5a67242ce06a67f802260e25ad5a0f1955d
Ben Zuba <zubes33@gmail.com>
2016-12-23 18:57:44 +0000

    draw_pieces_start

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,13 @@
 module GamesHelper
+  def render_piece(x, y)
+    pieces = Piece.all
+    puts pieces.size
+    pieces.each do |piece|
+      puts piece.type
+      if piece.x_position == x && piece.y_position == y
+        return "#{piece.type} #{piece.color}"
+      end
+    end
+    return ""
+  end
 end
