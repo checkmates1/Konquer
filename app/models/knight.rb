43ac053354994eb60412c2c3a6deed840f540db4
Ryan Wooff <ryanwooff@gmail.com>
2016-12-24 19:32:12 +0000

    more tests

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,12 +2,12 @@ class Knight < Piece
   def valid_move?(destination_x, destination_y)
     return false if x_position == destination_x
     return false if y_position == destination_y
-    valid_length?(destination_x, destination_y)
+    valid_move_distance?(destination_x, destination_y)
   end
 
   private
 
-  def valid_length?(destination_x, destination_y)
+  def valid_move_distance?(destination_x, destination_y)
     (x_position - destination_x).abs + (y_position - destination_y).abs == 3
   end
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -3,14 +3,28 @@ require 'rails_helper'
 RSpec.describe Knight, type: :model do
   describe '.valid_move?' do
     let(:game) { FactoryGirl.create(:game) }
-    let(:knight) { FactoryGirl.create(:knight, x_position: 1, y_position: 0, game: game) }
+    let(:knight) { FactoryGirl.create(:knight, x_position: 4, y_position: 4, game: game) }
 
-    context 'when not valid destination' do
-      it { expect(knight.valid_move?(3, 4)).to eq false }
+    context 'move is invalid' do
+      it { expect(knight.valid_move?(6, 6)).to eq false }
+      it { expect(knight.valid_move?(2, 7)).to eq false }
+      it { expect(knight.valid_move?(5, 3)).to eq false }
+      it { expect(knight.valid_move?(2, 7)).to eq false }
+      it { expect(knight.valid_move?(2, 6)).to eq false }
+      it { expect(knight.valid_move?(3, 3)).to eq false }
+      it { expect(knight.valid_move?(0, 0)).to eq false }
+      it { expect(knight.valid_move?(4, 4)).to eq false }
     end
 
-    context 'when valid' do
-      it { expect(knight.valid_move?(3, 1)).to eq true }
+    context 'move is valid' do
+      it { expect(knight.valid_move?(6, 5)).to eq true }
+      it { expect(knight.valid_move?(2, 3)).to eq true }
+      it { expect(knight.valid_move?(5, 6)).to eq true }
+      it { expect(knight.valid_move?(3, 2)).to eq true }
+      it { expect(knight.valid_move?(2, 5)).to eq true }
+      it { expect(knight.valid_move?(3, 6)).to eq true }
+      it { expect(knight.valid_move?(5, 2)).to eq true }
+      it { expect(knight.valid_move?(6, 3)).to eq true }
     end
   end
 end
