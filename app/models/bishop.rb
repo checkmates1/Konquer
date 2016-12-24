bba307ed6ae4b2366e23861f628569c3fdf195e4
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-12-24 04:14:12 +0000

    Create valid move for bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,17 @@
 class Bishop < Piece
+  def valid_move?(destination_x, destination_y)
+    x_difference = x_position - destination_x
+    y_difference = y_position - destination_y
+    return true if diagonal_move(destination_x, destination_y, x_difference, y_difference)
+    false
+  end
+
+  private
+
+  def diagonal_move(destination_x, destination_y, x_difference, y_difference)
+    return false unless x_difference.abs == y_difference.abs && !x_difference.zero?
+    return false unless (0..7).cover?(destination_x) && (0..7).cover?(destination_y)
+    return true unless diagonal_obstruction?(destination_x, destination_y, x_difference, y_difference)
+    false
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -20,4 +20,8 @@ FactoryGirl.define do
   factory :rook, parent: :piece, class: 'Rook' do
     type 'Rook'
   end
+
+  factory :bishop, parent: :piece, class: 'Bishop' do
+    type 'Bishop'
+  end
 end
