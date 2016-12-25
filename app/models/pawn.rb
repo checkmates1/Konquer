71bc78648fdeffd82b65af4f7a19cd671a299d1a
Ryan Wooff <ryanwooff@gmail.com>
2016-12-25 00:40:51 +0000

    valid pawn moves

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,7 +1,8 @@
 class Pawn < Piece
   def valid_move?(destination_x, destination_y)
     x_difference = x_position - destination_x
-    y_difference = destination_y - y_position
+    y_difference = destination_y - y_position if white?
+    y_difference = y_position - destination_y if black?
     return true if vertical_move(destination_y, x_difference, y_difference)
     return true if horizontal_move(destination_x, x_difference, y_difference)
     false
@@ -10,16 +11,20 @@ class Pawn < Piece
   private
 
   def vertical_move(destination_y, x_difference, y_difference)
-    return false unless y_difference == 1 && x_difference.zero?
-    return true unless vertical_obstruction?(destination_y, x_difference, y_difference)
+    return true if y_difference == 2 && first_move?
+    return true if y_difference == 1 && x_difference.zero?
+    return false if vertical_obstruction?(destination_y, x_difference, y_difference)
     false
   end
 
-  # how to test for pawns first move??
-  # capture logic???
   def horizontal_move(destination_x, x_difference, y_difference)
     return false unless y_difference.zero? && x_difference.abs == 1
     return false unless horizontal_obstruction?(destination_x, x_difference, y_difference)
     false
+  end
+
+  def first_move?
+    return true if y_position == 1 && white?
+    return true if y_position == 6 && black?
   end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Pawn, type: :model do
   describe '.valid_move?' do
     let(:game) { FactoryGirl.create(:game) }
-    let(:pawn) { FactoryGirl.create(:pawn, x_position: 4, y_position: 2, game: game) }
+    let(:pawn) { FactoryGirl.create(:pawn, x_position: 4, y_position: 2, color: 'white', game: game) }
 
     context 'when valid' do
       it { expect(pawn.valid_move?(4, 3)).to eq true }
@@ -24,12 +24,24 @@ RSpec.describe Pawn, type: :model do
     context 'when not valid destination' do
       it { expect(pawn.valid_move?(9, 0)).to eq false }
     end
-    # Need to test that it is an enemy
+    
     context 'when obstructed' do
       it 'returns true' do
-        FactoryGirl.create(:pawn, x_position: 4, y_position: 3, game: game)
-        expect(pawn.valid_move?(4, 3)).to eq true
+        FactoryGirl.create(:pawn, x_position: 4, y_position: 3, color: 'white', game: game)
+        expect(pawn.valid_move?(5, 3)).to eq false
       end
+    end
+  end
+end
+RSpec.describe Pawn, type: :model do
+  describe '.valid_move?' do
+    let(:game) { FactoryGirl.create(:game) }
+    let(:pawn) { FactoryGirl.create(:pawn, x_position: 4, y_position: 6, color: 'black', game: game) }
+
+    context 'when valid for black' do
+      it { expect(pawn.valid_move?(4, 4)).to eq true }
+      it { expect(pawn.valid_move?(4, 5)).to eq true }
+      it { expect(pawn.valid_move?(4, 7)).to eq false }
     end
   end
 end
