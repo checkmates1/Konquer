aded0657b79d9150db7c92d911c8c745f60dea35
Rosie <ariel.highsmith@gmail.com>
2016-12-26 08:03:38 -0800

    Reverse method in bishop.rb for readability

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -11,7 +11,7 @@ class Bishop < Piece
   def diagonal_move(destination_x, destination_y, x_difference, y_difference)
     return false unless x_difference.abs == y_difference.abs && !x_difference.zero?
     return false unless (0..7).cover?(destination_x) && (0..7).cover?(destination_y)
-    return true unless diagonal_obstruction?(destination_x, destination_y, x_difference, y_difference)
+    return false if diagonal_obstruction?(destination_x, destination_y, x_difference, y_difference)
     false
   end
 end
