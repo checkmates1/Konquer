f126f1dba9ad8381071828311caefea93ba1ba9e
Ryan Wooff <ryanwooff@gmail.com>
2016-12-27 06:11:32 -0800

    remove trailing whitespace

--- spec/factories.rb
+++ spec/factories.rb
@@ -24,7 +24,7 @@ FactoryGirl.define do
   factory :king, parent: :piece, class: 'King' do
     type 'King'
   end
-  
+
   factory :bishop, parent: :piece, class: 'Bishop' do
     type 'Bishop'
   end
