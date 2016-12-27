0039f2e9d1cf489d2367beae214e89a17b31ff0b
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-27 10:51:52 -0500

    adds method in games helper to get url for gravatar image

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,4 +3,9 @@ module GamesHelper
     piece = @game.pieces.find_by(x_position: x, y_position: y)
     return "#{piece.type} #{piece.color} " if piece
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
