3c6ae5e10065d1fd19827ca7dc8788edab026c4f
Ben Zuba <zubes33@gmail.com>
2016-12-28 23:30:39 +0000

    rough code

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,19 +2,26 @@ class PiecesController < ApplicationController
   def show
     @game =  Game.find(params[:id])
     @piece = Piece.find(params[:id])
-
+#not functional code just thinking through the process
+      first_selected_piece = @piece
+      first_selected_piece.update_attributes(x, y)
+    end
+    @piece.update_attributes.update_attributes(x, y)
   end
 
-  #   @piece.selected_updated_atrributes(x_position: x, y_position: y)
-  # end
-  # def update
-  #   @piece = Piece.find(params:id)
+  def update
+    @piece = Piece.find(params:id)
+    x = params[:x_position]
+    y = params[:y_position]
+    
+
+
 
 
   private
 
   def piece_params
-    params.require(:piece).permit(:show,:x_position, :y_position)
+    params.require(:piece).permit(:show, :x_position, :y_position)
   end
 
 end
