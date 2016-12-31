2ec2d6a7666fafa4894bd2404b4ff734fd325761
Ryan Wooff <ryanwooff@gmail.com>
2016-12-31 00:16:42 +0000

    small fix

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Game, type: :model do
     let(:king) { FactoryGirl.create(:king, x_position: 3, y_position: 0, game: game, color: 'white') }
 
     context 'when in check' do
-      it 'returns false' do
+      it 'returns true' do
         FactoryGirl.create(:pawn, x_position: 3, y_position: 1, game: game, color: 'black')
         expect(game.check?).to eq true
       end
