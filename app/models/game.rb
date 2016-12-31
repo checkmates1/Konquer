8709755a817032a7a605ac802f9552d439a53dd1
Ryan Wooff <ryanwooff@gmail.com>
2016-12-31 05:14:36 +0000

    fix remaining_pieces method

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,54 +8,47 @@ class Game < ActiveRecord::Base
   scope :available, -> { where('white_player_id IS NULL OR black_player_id IS NULL') }
 
   def populate_board!
-    #- white - 16
-    # Pawn - 8
     (0..7).each do |i|
       pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'white')
     end
-    # Rook - 2
     pieces.create(type: 'Rook', x_position: 0, y_position: 0, color: 'white')
     pieces.create(type: 'Rook', x_position: 7, y_position: 0, color: 'white')
-    # Knight - 2
     pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'white')
     pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'white')
-    # Bishop - 2
     pieces.create(type: 'Bishop', x_position: 2, y_position: 0, color: 'white')
     pieces.create(type: 'Bishop', x_position: 5, y_position: 0, color: 'white')
-    # Queen
     pieces.create(type: 'Queen', x_position: 4, y_position: 0, color: 'white')
-    # King
     pieces.create(type: 'King', x_position: 3, y_position: 0, color: 'white')
-    #- black - 16
-    # Pawn - 8
     (0..7).each do |i|
       pieces.create(type: 'Pawn', x_position: i, y_position: 6, color: 'black')
     end
-    # Rook - 2
     pieces.create(type: 'Rook', x_position: 0, y_position: 7, color: 'black')
     pieces.create(type: 'Rook', x_position: 7, y_position: 7, color: 'black')
-    # Knight - 2
     pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'black')
     pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'black')
-    # Bishop - 2
     pieces.create(type: 'Bishop', x_position: 2, y_position: 7, color: 'black')
     pieces.create(type: 'Bishop', x_position: 5, y_position: 7, color: 'black')
-    # Queen
     pieces.create(type: 'Queen', x_position: 3, y_position: 7, color: 'black')
-    # King
     pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'black')
   end
 
   def check?
-    king_is_in_check?('black') || king_is_in_check?('white')
+    kings_in_check?('white') || kings_in_check?('black')
   end
 
   private
 
-  def king_is_in_check?(color)
-    king = pieces.find_by(type: 'King', color: color)
-    enemy = remaining_pieces(switch_color(color)) # creates array of enemy pieces
+  def remaining_pieces(color) # creates an array of the remaining pieces w/desired color
+    pieces.includes(:game).where('color = ? and status = 1', color == 'white' ? 0 : 1).to_a
+  end
 
+  def opposite_color(color) # returns the opposite color
+    color == 'white' ? 'black' : 'white'
+  end
+
+  def kings_in_check?(color)
+    king = pieces.find_by(type: 'King', color: color)
+    enemy = remaining_pieces(opposite_color(color)) # creates array of enemy pieces
     enemy.each do |piece|
       if piece.valid_move?(king.x_position, king.y_position)
         @piece_in_check = piece # stores piece that has king in check
@@ -63,13 +56,5 @@ class Game < ActiveRecord::Base
       end
     end
     false
-  end
-
-  def remaining_pieces(color) # creates an array of the remaining pieces
-    pieces.includes(:game).where(color: color, status: 'alive').to_a
-  end
-
-  def switch_color(color) # returns the opposite color
-    color == 'white' ? 'black' : 'white'
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,20 +3,28 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe '.check?' do
     let(:game) { FactoryGirl.create(:game) }
-    let(:king) { FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game) }
 
-    context 'when in check' do
-      it 'should return true' do
-        FactoryGirl.create(:pawn, x_position: 3, y_position: 1, color: 'black', game: game)
-        expect(game.check?).to eq true
-      end
+    it 'should return true' do
+      FactoryGirl.create(:pawn, x_position: 4, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 3, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 2, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 3, y_position: 1, color: 'black', game: game)
+      FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game)
+      expect(game.check?).to eq true
     end
+  end
+end
 
-    context 'when not in check' do
-      it 'should return false' do
-        FactoryGirl.create(:pawn, x_position: 4, y_position: 4, color: 'black', game: game)
-        expect(game.check?).to eq false
-      end
+RSpec.describe Game, type: :model do
+  describe '.check?' do
+    let(:game) { FactoryGirl.create(:game) }
+
+    it 'should return false' do
+      FactoryGirl.create(:pawn, x_position: 4, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 3, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 2, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game)
+      expect(game.check?).to eq false
     end
   end
 end
