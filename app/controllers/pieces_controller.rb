ac35d5f33edce1c247f54a419e81ad79aa2b2025
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-12-31 18:14:15 -0800

    complete update method to move piece

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,25 +4,16 @@ class PiecesController < ApplicationController
     @game = @selected_piece.game
   end
 
-  # def show
-  #   @piece = Piece.find(piece_params)
-  # not functional code just thinking through the process
-  #   first_selected_piece = @piece
-  #   first_selected_piece.update_attributes(x, y)
-  # end
-  # @piece.update_attributes.update_attributes(x, y)
-  # end
-
   def update
     @selected_piece = Piece.find(params[:id])
     @selected_piece.update_attributes(piece_params)
-    redirect_to game_path(@selected_piece)
-    redirect_to game_path
+    redirect_to game_path(@selected_piece.game)
+    # redirect_to game_path
   end
 
   private
 
   def piece_params
-    params.require(:piece).permit(:x_position, :y_position)
+    params.permit(:x_position, :y_position)
   end
 end
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -4,12 +4,10 @@ module PiecesHelper
     return false unless @selected_piece.present?
     if @selected_piece.x_position == x && @selected_piece.y_position == y
       content_tag(:div, 'Selected piece', class: ['selected'])
-    elsif piece = @game.pieces.find_by(x_position: x, y_position: y)
-      link_to "#{piece.type} #{piece.color}", game_path(@game)
+    elsif piece 
+      link_to "#{piece.type} #{piece.color}", game_piece_path(@game, piece)
     else
-      link_to "move to", game_path(@game)
-
-
+      button_to("Move To", game_piece_path(@game, @selected_piece), method: "put", params: {x_position: x, y_position: y})
     end
   end
 end
