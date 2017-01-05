939a030fa33518cba79c915339587b858a6dd065
Ben Zuba <zubes33@gmail.com>
2017-01-05 23:20:48 +0000

    initial logic for check mate

--- app/models/game.rb
+++ app/models/game.rb
@@ -57,4 +57,16 @@ class Game < ActiveRecord::Base
     end
     false
   end
+
+  def check_mate?(color)
+    checkmate = true
+    while kings_in_check?
+      friendly = remaining_pieces(color)
+      friendly.each do |piece|
+        if piece.valid_move?
+          checkmate = false unless kings_in_check
+      end
+    end
+    return "CHECK MATE"
+  end
 end
