f3c5aa10ad78984d9cf4561c8b95f942bc6b79c6
Ben Zuba <zubes33@gmail.com>
2017-01-07 16:45:24 +0000

    added tests for check_mate

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -22,4 +22,27 @@ RSpec.describe Game, type: :model do
       expect(game.check?).to eq false
     end
   end
+
+
+  describe '.check_mate?' do
+    let(:game) { FactoryGirl.create(:game) }
+
+    it 'should return false' do
+      FactoryGirl.create(:rook, x_position: 2, y_position: 1, color: 'white', game: game)
+      FactoryGirl.create(:queen, x_position: 3, y_position: 1, color: 'black', game: game)
+      FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game)
+      expect(game.check_mate?).to eq false
+    end
+    let(:game) { FactoryGirl.create(:game) }
+
+    it 'should return true' do
+      FactoryGirl.create(:rook, x_position: 0, y_position: 1, color: 'black', game: game)
+      FactoryGirl.create(:queen, x_position: 3, y_position: 4, color: 'black', game: game)
+      FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game)
+      expect(game.check_mate?).to eq true
+    end
+  end
+
+
+
 end
