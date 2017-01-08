68c2bbe5b61d5dbaf2e76b5a3436556e50b3e223
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-01-08 16:05:15 -0500

    reconfigures tests for stalemate to include user assocs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -24,18 +24,21 @@ RSpec.describe Game, type: :model do
   end
 
   describe '.stalemate?' do
-    let(:game) { FactoryGirl.create(:game) }
+    let(:user_white) { FactoryGirl.create(:user) }
+    let(:user_black) { FactoryGirl.create(:user) }
+    let(:game) { FactoryGirl.create(:game, white_player: user_white, black_player: user_black, active_player: user_black) }
+
 
     it 'should return true' do
       FactoryGirl.create(:king, x_position: 0, y_position: 0, color: 'black', game: game)
-      FactoryGirl.create(:king, x_position: 2, y_position: 1, color: 'white', game: game)
+      FactoryGirl.create(:bishop, x_position: 2, y_position: 1, color: 'white', game: game)
       FactoryGirl.create(:queen, x_position: 1, y_position: 2, color: 'white', game: game)
       expect(game.stalemate?).to eq true
     end
 
     it 'should return false' do
       FactoryGirl.create(:king, x_position: 0, y_position: 0, color: 'black', game: game)
-      FactoryGirl.create(:king, x_position: 3, y_position: 2, color: 'white', game: game)
+      FactoryGirl.create(:bishop, x_position: 3, y_position: 2, color: 'white', game: game)
       FactoryGirl.create(:queen, x_position: 2, y_position: 3, color: 'white', game: game)
       expect(game.stalemate?).to eq false
     end
