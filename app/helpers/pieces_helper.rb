6d4cf34fbb7aff6b6989ea0df5fe95fdbf56094b
Ryan Wooff <ryanwooff@gmail.com>
2017-01-10 10:57:38 +0000

    slight fixes

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -3,7 +3,7 @@ module PiecesHelper
     piece = @game.pieces.find_by(x_position: x, y_position: y, game_id: @game.id)
     return false unless @selected_piece.present?
     if @selected_piece.x_position == x && @selected_piece.y_position == y
-      content_tag(:div, image_tag(img_name(@selected_piece)), class: ['selected responsive-img'])
+      content_tag(:div, image_tag(image_name(@selected_piece)), class: ['selected responsive-img'])
     elsif piece
       link_to image_tag(image_name(piece), class: 'responsive-img'), game_piece_path(@game, piece)
     else
