758308bdd91a6d15749959d6d53f195d870150ac
Ryan Wooff <ryanwooff@gmail.com>
2017-01-10 10:46:37 +0000

    changes piece names to icons

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,11 @@
 module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(x_position: x, y_position: y)
-    link_to "#{piece.type} #{piece.color}", game_piece_path(@game, piece) if piece
+    link_to image_tag(image_name(piece), class: 'responsive-img'), game_piece_path(@game, piece) if piece
+  end
+
+  def image_name(piece)
+    'pieces/' + piece.color + '-' + piece.type.downcase! + '.png'
   end
 
   def avatar_url(user)
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -3,11 +3,15 @@ module PiecesHelper
     piece = @game.pieces.find_by(x_position: x, y_position: y, game_id: @game.id)
     return false unless @selected_piece.present?
     if @selected_piece.x_position == x && @selected_piece.y_position == y
-      content_tag(:div, 'Selected piece', class: ['selected'])
+      content_tag(:div, image_tag(img_name(@selected_piece)), class: ['selected responsive-img'])
     elsif piece
-      link_to "#{piece.type} #{piece.color}", game_piece_path(@game, piece)
+      link_to image_tag(image_name(piece), class: 'responsive-img'), game_piece_path(@game, piece)
     else
       button_to('Move', game_piece_path(@game, @selected_piece), method: 'put', params: { x_position: x, y_position: y })
     end
+  end
+
+  def image_name(piece)
+    'pieces/' + piece.color + '-' + piece.type.downcase! + '.png'
   end
 end
