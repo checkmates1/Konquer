3b13ee782318089b74f6911f7a2851c19f5486be
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-01-10 08:48:57 -0800

    add test for assign_turn in game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -60,4 +60,20 @@ RSpec.describe Game, type: :model do
       expect(game.reload.winning_player).to eq white_player
     end
   end
+
+  describe 'assign_turn' do
+    let(:white_player) { FactoryGirl.create(:user) }
+    let(:black_player) { FactoryGirl.create(:user) }
+    let(:game) { FactoryGirl.create(:game, white_player: white_player, black_player: black_player) }
+
+    it 'assigns active_player to white_player if assign_turn method is called' do
+      game.assign_turn(white_player)
+      expect(game.reload.active_player).to eq white_player
+    end
+
+    it 'assigns active_player to black_player if assign_turn method is called' do
+      game.assign_turn(black_player)
+      expect(game.reload.active_player).to eq black_player
+    end
+  end
 end
