df7955ef99c4dc069d02d1e4650e5e66541b3de5
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-01-11 11:51:41 -0500

    adds routes, adds keys to devise config, adds auth controller, adds fb auth and methods to user model, adds link in devise views for fb login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'play#index'
   resources :games, only: [:new, :create, :show, :update] do
     member do
