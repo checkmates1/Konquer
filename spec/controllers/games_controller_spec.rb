463e4b30e5f74fc451e7a2090eb48fa4b79a89ed
Ben Zuba <zubes33@gmail.com>
2017-01-12 17:09:49 +0000

    added rspec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe 'check' do
+      let(:game) { FactoryGirl.create(:game) }
+
+      it 'should render flash notice if game in check' do
+      FactoryGirl.create(:pawn, x_position: 4, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 3, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 2, y_position: 2, color: 'black', game: game)
+      FactoryGirl.create(:pawn, x_position: 3, y_position: 1, color: 'black', game: game)
+      FactoryGirl.create(:king, x_position: 3, y_position: 0, color: 'white', game: game)
+      expect(game.check?).to eq true
+      expect(flash[:notice]).to be_present
+    end
+  end
 end
